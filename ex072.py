c2be6f93e2a7a7c964123135472c43dc3a6d6690
Danilo ohama <91641703+danilohama@users.noreply.github.com>
2023-09-14 21:19:50 -0300

    Add files via upload

--- ex072.py
+++ ex072.py
@@ -11,3 +11,12 @@ while True:
         break
         print(f'Tente novamente!', end='')
 print(f'Você digitou o número: \033[32m{count[num]}')
+resp = 'S'
+while True:
+    resp = str(input('\033[37mDeseja continuar\033[0m ? [\033[31mS\033[0m/\033[31mN\033[0m]: ')).upper()[0]
+    if resp == 'S':
+        num = int(input('Digite um número entre \033[33m0\033[0m e \033[33m20\033[0m: '))
+        print(f'Você digitou o número: \033[32m{count[num]}')
+    elif resp == 'N':
+        print('Operação finalizada!')
+        break
